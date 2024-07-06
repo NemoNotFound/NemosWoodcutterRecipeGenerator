920bebff97d6d0f0119d2ad52fcb89cc9d093fbd
NemoNotFound <02grimes-bard@icloud.com>
2024-07-06 21:35:09 +0200

    Add recipes for More Crafting Table, More Barrel Variants, More Chest Variants, More Composter Variants

--- src/main/java/com/nemonotfound/nemoswoodcutter/datagen/RecipeGenerator.java
+++ src/main/java/com/nemonotfound/nemoswoodcutter/datagen/RecipeGenerator.java
@@ -4,6 +4,10 @@ import biomesoplenty.api.block.BOPBlocks;
 import biomesoplenty.api.item.BOPItems;
 import biomesoplenty.init.ModTags;
 import com.mojang.datafixers.util.Pair;
+import com.pnku.mblv.init.MblvItemInit;
+import com.pnku.mcmv.init.McmvItemInit;
+import io.github.lieonlion.lolmct.init.MctItemInit;
+import io.github.lieonlion.mcv.init.McvItemInit;
 import io.github.uhq_games.regions_unexplored.block.RuBlocks;
 import io.github.uhq_games.regions_unexplored.data.tags.RuTags;
 import io.github.uhq_games.regions_unexplored.item.RuItems;
@@ -12,12 +16,14 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.fabricmc.fabric.api.resource.conditions.v1.DefaultResourceConditions;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
@@ -32,6 +38,110 @@ public class RecipeGenerator extends FabricRecipeProvider {
         createBiomesOPlentyRecipes(exporter);
         createRegionsUnexploredRecipes(exporter);
         createBetterArcheologyRecipes(exporter);
+        createMoreBarrelVariantsRecipes(exporter);
+        createMoreChestVariantsRecipes(exporter);
+        createMoreCraftingTablesRecipes(exporter);
+        createMoreComposterVariantsRecipes(exporter);
+    }
+
+    private void createMoreComposterVariantsRecipes(RecipeExporter exporter) {
+        RecipeExporter exporterWithCondition = withConditions(exporter, DefaultResourceConditions.allModsLoaded("lolmcmv"));
+        createCarpentryRecipe(exporterWithCondition, ItemTags.ACACIA_LOGS, 1, McmvItemInit.ACACIA_COMPOSTER_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.ACACIA_PLANKS, 1), McmvItemInit.ACACIA_COMPOSTER_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.BAMBOO_BLOCKS, 1, McmvItemInit.BAMBOO_COMPOSTER_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.BAMBOO_PLANKS, 1), McmvItemInit.BAMBOO_COMPOSTER_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.BIRCH_LOGS, 1, McmvItemInit.BIRCH_COMPOSTER_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.BIRCH_PLANKS, 1), McmvItemInit.BIRCH_COMPOSTER_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.CHERRY_LOGS, 1, McmvItemInit.CHERRY_COMPOSTER_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.CHERRY_PLANKS, 1), McmvItemInit.CHERRY_COMPOSTER_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.CRIMSON_STEMS, 1, McmvItemInit.CRIMSON_COMPOSTER_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.CRIMSON_PLANKS, 1), McmvItemInit.CRIMSON_COMPOSTER_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.DARK_OAK_LOGS, 1, McmvItemInit.DARK_OAK_COMPOSTER_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.DARK_OAK_PLANKS, 1), McmvItemInit.DARK_OAK_COMPOSTER_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.JUNGLE_LOGS, 1, McmvItemInit.JUNGLE_COMPOSTER_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.JUNGLE_PLANKS, 1), McmvItemInit.JUNGLE_COMPOSTER_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.MANGROVE_LOGS, 1, McmvItemInit.MANGROVE_COMPOSTER_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.MANGROVE_PLANKS, 1), McmvItemInit.MANGROVE_COMPOSTER_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.OAK_LOGS, 1, McmvItemInit.OAK_COMPOSTER_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.OAK_PLANKS, 1), McmvItemInit.OAK_COMPOSTER_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.SPRUCE_LOGS, 1, McmvItemInit.SPRUCE_COMPOSTER_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.SPRUCE_PLANKS, 1), McmvItemInit.SPRUCE_COMPOSTER_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.WARPED_STEMS, 1, McmvItemInit.WARPED_COMPOSTER_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.WARPED_PLANKS, 1), McmvItemInit.WARPED_COMPOSTER_I, 1);
+    }
+
+    private void createMoreCraftingTablesRecipes(RecipeExporter exporter) {
+        RecipeExporter exporterWithCondition = withConditions(exporter, DefaultResourceConditions.allModsLoaded("lolmct"));
+        createCarpentryRecipe(exporterWithCondition, ItemTags.ACACIA_LOGS, 1, MctItemInit.ACACIA_TABLE_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.ACACIA_PLANKS, 1), MctItemInit.ACACIA_TABLE_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.BAMBOO_BLOCKS, 1, MctItemInit.BAMBOO_TABLE_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.BAMBOO_PLANKS, 1), MctItemInit.BAMBOO_TABLE_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.BIRCH_LOGS, 1, MctItemInit.BIRCH_TABLE_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.BIRCH_PLANKS, 1), MctItemInit.BIRCH_TABLE_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.CHERRY_LOGS, 1, MctItemInit.CHERRY_TABLE_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.CHERRY_PLANKS, 1), MctItemInit.CHERRY_TABLE_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.CRIMSON_STEMS, 1, MctItemInit.CRIMSON_TABLE_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.CRIMSON_PLANKS, 1), MctItemInit.CRIMSON_TABLE_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.DARK_OAK_LOGS, 1, MctItemInit.DARK_OAK_TABLE_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.DARK_OAK_PLANKS, 1), MctItemInit.DARK_OAK_TABLE_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.JUNGLE_LOGS, 1, MctItemInit.JUNGLE_TABLE_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.JUNGLE_PLANKS, 1), MctItemInit.JUNGLE_TABLE_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.MANGROVE_LOGS, 1, MctItemInit.MANGROVE_TABLE_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.MANGROVE_PLANKS, 1), MctItemInit.MANGROVE_TABLE_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.SPRUCE_LOGS, 1, MctItemInit.SPRUCE_TABLE_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.SPRUCE_PLANKS, 1), MctItemInit.SPRUCE_TABLE_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.WARPED_STEMS, 1, MctItemInit.WARPED_TABLE_I, 4);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.WARPED_PLANKS, 1), MctItemInit.WARPED_TABLE_I, 1);
+    }
+
+    private void createMoreChestVariantsRecipes(RecipeExporter exporter) {
+        RecipeExporter exporterWithCondition = withConditions(exporter, DefaultResourceConditions.allModsLoaded("lolmcv"));
+        createCarpentryRecipe(exporterWithCondition, ItemTags.ACACIA_LOGS, 1, McvItemInit.ACACIA_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.ACACIA_PLANKS, 4), McvItemInit.ACACIA_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.BAMBOO_BLOCKS, 1, McvItemInit.BAMBOO_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.BAMBOO_PLANKS, 4), McvItemInit.BAMBOO_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.BIRCH_LOGS, 1, McvItemInit.BIRCH_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.BIRCH_PLANKS, 4), McvItemInit.BIRCH_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.CHERRY_LOGS, 1, McvItemInit.CHERRY_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.CHERRY_PLANKS, 4), McvItemInit.CHERRY_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.CRIMSON_STEMS, 1, McvItemInit.CRIMSON_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.CRIMSON_PLANKS, 4), McvItemInit.CRIMSON_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.DARK_OAK_LOGS, 1, McvItemInit.DARK_OAK_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.DARK_OAK_PLANKS, 4), McvItemInit.DARK_OAK_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.JUNGLE_LOGS, 1, McvItemInit.JUNGLE_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.JUNGLE_PLANKS, 4), McvItemInit.JUNGLE_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.MANGROVE_LOGS, 1, McvItemInit.MANGROVE_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.MANGROVE_PLANKS, 4), McvItemInit.MANGROVE_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.OAK_LOGS, 1, McvItemInit.OAK_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.OAK_PLANKS, 4), McvItemInit.OAK_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.SPRUCE_LOGS, 1, McvItemInit.SPRUCE_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.SPRUCE_PLANKS, 4), McvItemInit.SPRUCE_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.WARPED_STEMS, 1, McvItemInit.WARPED_CHEST_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.WARPED_PLANKS, 4), McvItemInit.WARPED_CHEST_I, 1);
+    }
+
+    private void createMoreBarrelVariantsRecipes(RecipeExporter exporter) {
+        RecipeExporter exporterWithCondition = withConditions(exporter, DefaultResourceConditions.allModsLoaded("lolmblv"));
+        createCarpentryRecipe(exporterWithCondition, ItemTags.ACACIA_LOGS, 1, MblvItemInit.ACACIA_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.ACACIA_PLANKS, 4), MblvItemInit.ACACIA_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.BAMBOO_BLOCKS, 1, MblvItemInit.BAMBOO_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.BAMBOO_PLANKS, 4), MblvItemInit.BAMBOO_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.BIRCH_LOGS, 1, MblvItemInit.BIRCH_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.BIRCH_PLANKS, 4), MblvItemInit.BIRCH_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.CHERRY_LOGS, 1, MblvItemInit.CHERRY_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.CHERRY_PLANKS, 4), MblvItemInit.CHERRY_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.CRIMSON_STEMS, 1, MblvItemInit.CRIMSON_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.CRIMSON_PLANKS, 4), MblvItemInit.CRIMSON_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.DARK_OAK_LOGS, 1, MblvItemInit.DARK_OAK_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.DARK_OAK_PLANKS, 4), MblvItemInit.DARK_OAK_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.JUNGLE_LOGS, 1, MblvItemInit.JUNGLE_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.JUNGLE_PLANKS, 4), MblvItemInit.JUNGLE_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.MANGROVE_LOGS, 1, MblvItemInit.MANGROVE_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.MANGROVE_PLANKS, 4), MblvItemInit.MANGROVE_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.OAK_LOGS, 1, MblvItemInit.OAK_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.OAK_PLANKS, 4), MblvItemInit.OAK_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, ItemTags.WARPED_STEMS, 1, MblvItemInit.WARPED_BARREL_I, 1);
+        createCarpentryRecipe(exporterWithCondition, Pair.of(Blocks.WARPED_PLANKS, 4), MblvItemInit.WARPED_BARREL_I, 1);
     }
 
     private void createBiomesOPlentyRecipes(RecipeExporter exporter) {
@@ -913,25 +1023,23 @@ public class RecipeGenerator extends FabricRecipeProvider {
     private void createCarpentryRecipe(RecipeExporter exporter, Pair<Block, Integer> ingredientPair, ItemConvertible output,
                                        int outputCount) {
         Block input = ingredientPair.getFirst();
-        Identifier blockId =  Registries.BLOCK.getId(input);
-        String blockNamespace = blockId.getNamespace();
-        String blockName = blockId.getPath();
+        String blockName = Registries.BLOCK.getId(input).getPath();
+        String namespace = Registries.ITEM.getId(output.asItem()).getNamespace();
 
         WoodcuttingRecipeJsonBuilder.create(RecipeCategory.DECORATIONS, output, outputCount)
                 .input(Ingredient.ofItems(input), ingredientPair.getSecond())
                 .criterion(FabricRecipeProvider.hasItem(output), conditionsFromItem(output))
-                .offerTo(exporter, new Identifier(blockNamespace + "_" + blockName + "_to_" + getRecipeName(output) + "_woodcutting"));
+                .offerTo(exporter, new Identifier(namespace + "_" + blockName + "_to_" + getRecipeName(output) + "_woodcutting"));
     }
 
     private void createCarpentryRecipe(RecipeExporter exporter, TagKey<Item> tag, Integer ingredientCount, ItemConvertible output,
                                        int outputCount) {
-        Identifier tagId = tag.id();
-        String tagNamespace = tagId.getNamespace();
-        String tagPath = tagId.getPath();
+        String tagPath = tag.id().getPath();
+        String namespace = Registries.ITEM.getId(output.asItem()).getNamespace();
 
         WoodcuttingRecipeJsonBuilder.create(RecipeCategory.DECORATIONS, output, outputCount)
                 .input(Ingredient.fromTag(tag), ingredientCount)
                 .criterion(FabricRecipeProvider.hasItem(output), conditionsFromItem(output))
-                .offerTo(exporter, new Identifier(tagNamespace + "_" + tagPath + "_to_" + getRecipeName(output) + "_woodcutting"));
+                .offerTo(exporter, new Identifier(namespace + "_" + tagPath + "_to_" + getRecipeName(output) + "_woodcutting"));
     }
 }
